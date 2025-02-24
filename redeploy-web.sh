f8d3d085f1955053185ad8620aaadc8485834682
cufta22 <cufta22@proton.me>
2025-02-25 00:32:33 +0100

    change redeploy script

--- redeploy-web.sh
+++ redeploy-web.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Set default environment to 'staging' if no argument is provided
+ENVIRONMENT=${1:-staging}
+
+# Determine the PM2 instance name based on the environment argument
+if [ "$ENVIRONMENT" == "staging" ]; then
+  INSTANCE_NAME="web-staging"
+elif [ "$ENVIRONMENT" == "production" ]; then
+  INSTANCE_NAME="web-production"
+else
+  echo "Invalid environment specified. Use 'staging' or 'production'. Defaulting to 'staging'."
+  INSTANCE_NAME="web-staging"
+fi
+
+# Stop pm2 process
+pm2 stop $INSTANCE_NAME
+
 # Fetch latest code
 git pull
 
@@ -9,7 +25,6 @@ pnpm install
 pnpm run build
 
 # Restart pm2 process
-pm2 restart web-production
-pm2 restart web-staging
+pm2 restart $INSTANCE_NAME
 
-sudo systemctl reload nginx
+systemctl reload nginx
