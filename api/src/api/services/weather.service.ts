26c399ab77ac8d84855064ff39bc3143c6f22363
Undertaker-afk <flogamer008@gmx.de>
2025-03-03 11:18:49 +0100

    Update weather.service.ts

--- api/src/api/services/weather.service.ts
+++ api/src/api/services/weather.service.ts
@@ -6,13 +6,10 @@ export const handleGetWeather = async (ctx: Context) => {
 
   const lat = searchParams.get("lat") || "1";
   const lon = searchParams.get("lon") || "1";
-  const units = searchParams.get("units") || "metric";
 
-  const OPEN_WEATHER_URL = process.env.OPEN_WEATHER_URL;
-  const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
-
+  const OPEN_METEO_URL = "https://api.open-meteo.com/v1/forecast";
   const res = await fetch(
-    `${OPEN_WEATHER_URL}/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${OPEN_WEATHER_API_KEY}&units=${units}`
+    `${OPEN_METEO_URL}?latitude=${lat}&longitude=${lon}&hourly=temperature_2m`
   );
 
   const resData = await res.json();
