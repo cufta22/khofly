80843cfec68d44381673c1b617041d31c2d2e2fa
Undertaker-afk <flogamer008@gmx.de>
2025-03-03 11:40:03 +0100

    Update weather.service.ts
    Added the rain var to the forecast

--- api/src/api/services/weather.service.ts
+++ api/src/api/services/weather.service.ts
@@ -9,7 +9,7 @@ export const handleGetWeather = async (ctx: Context) => {
 
   const OPEN_METEO_URL = "https://api.open-meteo.com/v1/forecast";
   const res = await fetch(
-    `${OPEN_METEO_URL}?latitude=${lat}&longitude=${lon}&hourly=temperature_2m`
+    `${OPEN_METEO_URL}?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,`
   );
 
   const resData = await res.json();
