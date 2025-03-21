6b3f37bd5f5de2647686e2f8154dced9c7ff4487
cufta22 <cufta22@proton.me>
2025-03-22 00:52:58 +0100

    fix ip endpoint

--- api/src/api/services/ip.service.ts
+++ api/src/api/services/ip.service.ts
@@ -7,7 +7,7 @@ export const handleGetIP = async (ctx: Context) => {
   // Try to get IP from headers
   const myIP =
     ctx.request.headers.get("x-real-ip") ||
-    ctx.request.headers.get("x-forwarder-for") ||
+    ctx.request.headers.get("x-forwarded-for") ||
     "No IP found";
 
   return { error: false, message: "Request IP", data: myIP };
