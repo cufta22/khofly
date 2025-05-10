02b45a9a6a35ffae5e50d6eba9f76e4c7d0c5981
cufta22 <cufta22@proton.me>
2025-05-11 01:59:22 +0200

    Organize test

--- web/src/modules/Search/components/components/Organize/components/OCurrent.tsx
+++ web/src/modules/Search/components/components/Organize/components/OCurrent.tsx
@@ -1,8 +1,8 @@
-import { Accordion, SimpleGrid, Text } from "@mantine/core";
+import { Accordion, Center, SimpleGrid, Text } from "@mantine/core";
 import { IconLink } from "@tabler/icons-react";
 import classes from "../styles.module.scss";
 import useSearchQuery from "@hooks/use-search-query";
-import { useSWRConfig } from "swr";
+import { type State, useSWRConfig } from "swr";
 import { useEffect, useState } from "react";
 import type { ISearXNGResultsGeneral } from "@ts/searxng.types";
 import DomainItem from "./DomainItem";
@@ -11,27 +11,55 @@ import { removeSubdomain } from "./utils";
 const OCurrent = () => {
   const q = useSearchQuery();
 
-  const { cache, mutate, ...extraConfig } = useSWRConfig();
+  const { cache } = useSWRConfig();
   const cacheKeys = Array.from(cache.keys());
-  console.log(cacheKeys);
 
   const [currentDomains, setCurrentDomains] = useState<string[]>([]);
 
   useEffect(() => {
-    if (currentDomains.length) return;
+    console.log("Cache keys");
+    console.log(cacheKeys);
 
+    if (currentDomains.length) return;
+    console.log("Step 1");
+    console.log(`currentDomains length: ${currentDomains.length}`);
+
+    // Find the current cacke key with the matching query
     const findPageDataKey = cacheKeys
       .filter((item) => item.startsWith("$inf$/search?q="))
       .find((key) => {
         const keyURL = new URL(`http://example.com${key}`);
 
-        return keyURL.searchParams.get("q")?.includes(q);
+        return (
+          keyURL.searchParams.get("q")?.includes(q) &&
+          keyURL.searchParams.get("categories") === "general"
+        );
       });
-
     if (!findPageDataKey) return;
 
-    const pageData = cache.get(findPageDataKey)?.data?.[0]?.results;
+    console.log("Step 2");
+    console.log(`findPageDataKey: ${findPageDataKey}`);
+
+    const cacheHit = cache.get(findPageDataKey) as State<ISearXNGResultsGeneral[]>;
+    if (!cacheHit.data) return;
+
+    console.log("Step 3");
+    console.log(`cacheHit data length: ${cacheHit.data.length}`);
+
+    // Extract all the results from the
+    const pageData = cacheHit.data?.reduce<ISearXNGResultsGeneral["results"][0][]>(
+      (accumulator, currentObject) => {
+        if (currentObject?.results) {
+          accumulator.push(...currentObject.results);
+        }
+        return accumulator;
+      },
+      []
+    );
     if (!pageData) return;
+
+    console.log("Step 4");
+    console.log(`pageData length: ${pageData.length}`);
 
     const uniqueDomains: string[] = pageData.map((result: ISearXNGResultsGeneral["results"][0]) => {
       return removeSubdomain(result?.parsed_url?.[1]);
@@ -46,11 +74,17 @@ const OCurrent = () => {
         <Text size="lg">Current domains</Text>
       </Accordion.Control>
       <Accordion.Panel>
-        <SimpleGrid mt="lg" cols={2} spacing="md">
-          {currentDomains.map((item) => (
-            <DomainItem key={item} domain={item} isCurrent />
-          ))}
-        </SimpleGrid>
+        {currentDomains.length ? (
+          <SimpleGrid mt="lg" cols={2} spacing="md">
+            {currentDomains.map((item) => (
+              <DomainItem key={item} domain={item} isCurrent />
+            ))}
+          </SimpleGrid>
+        ) : (
+          <Center>
+            <Text>Wow, such empty</Text>
+          </Center>
+        )}
       </Accordion.Panel>
     </Accordion.Item>
   );
