463f2051ebd6fc33dff147920e92d52ded63456d
cufta22 <cufta22@proton.me>
2025-05-12 23:46:40 +0200

    fix organize

--- web/src/modules/Search/components/components/Organize/components/OCurrent.tsx
+++ web/src/modules/Search/components/components/Organize/components/OCurrent.tsx
@@ -1,58 +1,11 @@
 import { Accordion, Center, SimpleGrid, Text } from "@mantine/core";
 import { IconLink } from "@tabler/icons-react";
 import classes from "../styles.module.scss";
-import useSearchQuery from "@hooks/use-search-query";
-import { type State, useSWRConfig } from "swr";
-import { useEffect, useState } from "react";
-import type { ISearXNGResultsGeneral } from "@ts/searxng.types";
 import DomainItem from "./DomainItem";
-import { removeSubdomain } from "./utils";
+import { useSearchStore } from "@store/search";
 
 const OCurrent = () => {
-  const q = useSearchQuery();
-
-  const { cache } = useSWRConfig();
-  const cacheKeys = Array.from(cache.keys());
-
-  const [currentDomains, setCurrentDomains] = useState<string[]>([]);
-
-  useEffect(() => {
-    if (currentDomains.length) return;
-
-    // Find the current cacke key with the matching query
-    const findPageDataKey = cacheKeys
-      .filter((item) => item.startsWith("$inf$/search?q="))
-      .find((key) => {
-        const keyURL = new URL(`http://example.com${key}`);
-
-        return (
-          keyURL.searchParams.get("q")?.includes(q) &&
-          keyURL.searchParams.get("categories") === "general"
-        );
-      });
-    if (!findPageDataKey) return;
-
-    const cacheHit = cache.get(findPageDataKey) as State<ISearXNGResultsGeneral[]>;
-    if (!cacheHit.data) return;
-
-    // Extract all the results from the
-    const pageData = cacheHit.data?.reduce<ISearXNGResultsGeneral["results"][0][]>(
-      (accumulator, currentObject) => {
-        if (currentObject?.results) {
-          accumulator.push(...currentObject.results);
-        }
-        return accumulator;
-      },
-      []
-    );
-    if (!pageData) return;
-
-    const uniqueDomains: string[] = pageData.map((result: ISearXNGResultsGeneral["results"][0]) => {
-      return removeSubdomain(result?.parsed_url?.[1]);
-    });
-
-    setCurrentDomains([...new Set(uniqueDomains)]);
-  }, [q, cacheKeys]);
+  const domainsCurrent = useSearchStore((state) => state.domainsCurrent);
 
   return (
     <Accordion.Item className={classes.acc_item} value="current">
@@ -60,9 +13,9 @@ const OCurrent = () => {
         <Text size="lg">Current domains</Text>
       </Accordion.Control>
       <Accordion.Panel>
-        {currentDomains.length ? (
+        {domainsCurrent.length ? (
           <SimpleGrid mt="lg" cols={2} spacing="md">
-            {currentDomains.map((item) => (
+            {domainsCurrent.map((item) => (
               <DomainItem key={item} domain={item} isCurrent />
             ))}
           </SimpleGrid>
